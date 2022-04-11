d544840f3530316088a6c23c0b2be34f771e5d26
Asheton S. M <AshetonSM@gmail.com>
2022-04-11 09:57:17 -0500

    Added React-router-dom, rudimentary routing is non-functional.

--- jahm-frontend/src/App.js
+++ jahm-frontend/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Login from "./features/Login";
+import Profile from "./features/Profile";
+
 import './App.css';
 
 function App() {
   return (
+    <>
+    <BrowserRouter>
     <div className="App">
       <h1>JAHM Pokemon Quiz</h1>
+      <Link to="/">Home</Link> {" | "}
+      <Link to="/profile">Profile</Link>
     </div>
+    <Routes>
+      <Route path="Login" element={<Login />}/>
+      <Route path="Profile" element={<Profile/>}/>
+    </Routes>
+  </BrowserRouter>
+    </>
   );
 }
 
