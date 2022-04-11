0addb3552db2bb81f38445c7b4f36f9ad07c53d3
Asheton S. M <AshetonSM@gmail.com>
2022-04-11 11:04:37 -0500

    PlayButton now randomly chooses and gets the data of the correctPokemon.

--- jahm-frontend/src/features/PlayButton.js
+++ jahm-frontend/src/features/PlayButton.js
@@ -12,14 +12,39 @@ const PlayButton = props => {
      * Random min/max format: (MAX - MIN + 1) + MIN
      */
     function startGame() {
+        const startButton = document.getElementById("startButton");
+        startButton.disabled = true;
         for (let i = 0; i < 5; i++) {
             pokemonChoices.push(Math.floor(Math.random() * (809 - 1 + 1) + 1));
         }
+        // Randomly assign a correct pokemon out of the possible choices
+        correctPokemon = pokemonChoices.at(Math.floor(Math.random() * (5 - 1 + 1) + 1));
+
+        // DEBUG INFO
         console.log("Game started. " + pokemonChoices);
+        console.log("The answer is: " + correctPokemon);
+
+        getPokemonData();
     }
 
+    async function getPokemonData() {
+
+        // Randomly assign a correct pokemon out of the possible choices
+        correctPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonChoices.at(Math.floor(Math.random() * (5 - 1 + 1) + 1)))
+          .then((response) => response.json())
+          .catch((error) => console.log(error))
+          .then ((data) => populatePokemonData(data));
+      }
+
+      // TODO: Get the names of each other pokemon in pokemonChoices
+
+      function populatePokemonData(data) {
+          console.log(data);
+          console.log("The correct pokemon is: " + data.name);
+      }
+
     return (
-        <button value="submit" onClick={startGame}>Start!</button>
+        <button value="submit" id="startButton" onClick={startGame}>Start!</button>
     );
 }
 
