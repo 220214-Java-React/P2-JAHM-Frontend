1b9e4c38ff6baf2878f5926f94bb04349b847296
Jacob Williams <jacob975@revature.net>
2022-04-13 10:06:29 -0500

    refactored quiz main to better fit REACT design, allow selection of both quiz type and quiz difficulty.

---
+++ jahm-frontend/src/features/quiz/timed.js
@@ -0,0 +1,14 @@
+export default function TimedQuiz ()
+{
+    let totalQuizTime;
+    let timeBetweenHints;
+    let scoreMultiplier;
+
+    return(
+        <div className = "pokeQuiz">
+         
+            
+        </div>
+    );
+ 
+}
