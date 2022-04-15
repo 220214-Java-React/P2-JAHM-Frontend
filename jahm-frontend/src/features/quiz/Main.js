86aedb96589e46017c37336b6006de2da0013d15
Asheton S. M <AshetonSM@gmail.com>
2022-04-15 16:18:58 -0500

    Added gamePlaying state, result, pokemonChoice, and correctPokemon variables. Added pokemonChoice population. Added correctPokemon selection to populatePokemonChoice. Added changeGameState function.

--- jahm-frontend/src/features/quiz/Main.js
+++ jahm-frontend/src/features/quiz/Main.js
@@ -2,22 +2,50 @@ import React from "react";
 import Quiz from "./Quiz.js";
 import Settings from "./Settings";
 import { useState } from "react";
+import PlayButton from "../PlayButton.js";
 
 function Main(props) {
 
     const [qType, setType] = useState("");
     const [qDiff, setDiff] = useState("");
-    let selected = false;
+    const [gamePlaying, setGamePlaying] = useState(false);
+    var result = "";
+    let pokemonChoice = [];
+    var correctPokemon;
 
     function changeDifficulty(e) { setDiff(e); }
 
     function changeType(e) { setType(e); }
 
-    return <>
-        {(qType !== "" && qDiff !== "") ? <Quiz quizType={qType} quizDifficulty={qDiff} typeSet = {setType} diffSet = {setDiff} user = {props.User}/>
-            :
-            <Settings difficulty={changeDifficulty} type={changeType} />}
+    function populatePokemonChoice(props) {
+        console.log("Main added " + props.name + " to the list of Pokemon choices.");
+        pokemonChoice.push(props);
 
+        // Randomly set the correct pokemon out of the 5 that have now been added.
+        if (pokemonChoice.length > 4) {
+            correctPokemon = pokemonChoice.at(Math.floor(Math.random() * (4 - 1 + 1) + 1));
+            console.log("Main set the correct pokemon to: " + correctPokemon.name);
+            // Game is now running
+            changeGameState(true);
+        }
+    }
+
+    function changeGameState(newState) {
+        setGamePlaying(newState);
+    }
+
+    /**
+     * If game is NOT playing, and if user has NOT selected qType and qDifficulty, display the PlayButton.
+     * If user has clicked PlayButton, pokemonChoice and correctPokemon are not null.
+     * Thus, display quiz.
+     */
+    return <>
+
+        { gamePlaying == false ? <Settings difficulty={changeDifficulty} type={changeType} /> : null }
+        { qDiff !== "" && qType !== "" && gamePlaying !== true ?  <PlayButton populatePokemonChoice={populatePokemonChoice} /> : null }
+        { gamePlaying === true ?
+
+            <Quiz quizType={qType} quizDifficulty={qDiff} typeSet={setType} diffSet={setDiff} user={props.User} /> : null }
 
     </>;
 
