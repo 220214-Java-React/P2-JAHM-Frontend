450f2ad32c8bcfc4ee21d77a505986a8ec9a7121
HalimUzodike <halimuzodike@gmail.com>
2022-04-15 22:35:43 -0400

    Login feature added. Resolving conflicts.

--- jahm-frontend/src/App.js
+++ jahm-frontend/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import PlayButton from "./features/PlayButton";
-import Login from "./features/Login";
+import Login from './features/components/account/Login';
 import Profile from "./features/Profile";
 
 import './App.css';
+import Register from './features/components/account/Register';
 
 function App() {
   return (
     <>
     <BrowserRouter>
     <div className="App">
+      <Login />
+      <Register />
       <h1>JAHM Pokemon Quiz</h1>
       <Link to="/">Home</Link> {" | "}
       <Link to="/profile">Profile</Link>
@@ -20,12 +23,12 @@ function App() {
       <PlayButton/>
     </div>
     <Routes>
-      <Route path="Login" element={<Login />}/>
+      <Route path="login" element={<login />}/>
+      
       <Route path="Profile" element={<Profile/>}/>
-      <Route path='/' exact component={Home} />
-      <Route path='/quiz' component={Quiz} />
+      
       <Route path='/history' component={History} />
-      <Route path='/logout' component={Logout} />
+      
     </Routes>
   </BrowserRouter>
     </>
