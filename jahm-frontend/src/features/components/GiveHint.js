9e9b0b01cfd1756f9c24de79daecffba4dd381df
Asheton S. M <AshetonSM@gmail.com>
2022-04-18 16:48:53 -0500

    Moved responsibility of gameplaying away from GiveHint into Quiz.js. Deleted old comments. Added conditional if no hints are remaining.

--- jahm-frontend/src/features/components/GiveHint.js
+++ jahm-frontend/src/features/components/GiveHint.js
@@ -1,15 +1,11 @@
 import React, {useState} from 'react';
 
 const GiveHint = props => {
+    const hint = ["Weight", "Height"];
 
     function getHint() {
-        if (props.gamePlaying && props.hintsRemaining > 0) {
-
-            // <Game/> Should contain:
-            // playerHints[]: a list of possible hints
-            // newHint(): a function that chooses a new hint, appends it to playerHints[], and subtracts from the score
-            // numberOfHints int: number of hints received thus far
-            props.newHint();
+        if (props.hintNumber >= 0) {
+            props.newHint(hint[props.hintNumber]);
         }
     }
 
